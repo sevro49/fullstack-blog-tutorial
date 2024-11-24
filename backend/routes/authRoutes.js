bda108aba4ec9a4cd6b21e7ed1e5a7c7f18d5c51
sevro49 <95761902+sevro49@users.noreply.github.com>
2024-11-24 16:25:34 +0300

    feat: added refreshToken

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { register, login } = require('../controllers/authController');
+const { register, login, generateAccessToken, logout } = require('../controllers/authController');
 
 const router = express.Router();
 
 // Routes for authentication
 router.post('/register', register); // register a user
 router.post('/login', login); // login a user
+router.post('/refresh-token', generateAccessToken); // generate access token
+router.post('/logout', logout); // logout a user
 
 module.exports = router;
